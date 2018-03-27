16d01333467f17712d5e9cf2a6cdf5f0ee14ad07
Unknown <kristian.schlikow@web.de>
2018-03-28 00:44:37 +0200

    Removed unnecessary files
    With the last change to the structure, a lot of files have become obsolete. This commit gets rid of any files no longer in use.

--- CSV_Parser/item.cpp
+++
@@ -1,3 +0,0 @@
-#include "item.h"
-
-float item::tvalue() { return (amount * value); }
--- CSV_Parser/main.h
+++
@@ -1,7 +0,0 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-#include <csv_parser.h>
-#include <includes.h>
-
-#endif // MAIN_H
